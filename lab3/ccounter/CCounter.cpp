ba0e39ddd868c5b493c9677d116e04da9daccf92
biekin <kinga.kb@interia.pl>
2017-03-14 12:33:32 +0100

    lab3

---
+++ lab3/ccounter/CCounter.cpp
@@ -0,0 +1,22 @@
+//
+// Created by Kinga on 14.03.2017.
+//
+
+#include "CCounter.h"
+
+std::unique_ptr<Counter> Init()
+{
+
+}
+void Inc(std::string key, std::unique_ptr<Counter> *counter)
+{
+
+}
+int Counts(const std::unique_ptr<Counter> &counter, std::string key)
+{
+
+}
+void SetCountsTo(std::string key, int value, std::unique_ptr<Counter> *counter)
+{
+
+}
---
+++ lab3/minimaltimedifference/MinimalTimeDifference.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Kinga on 14.03.2017.
+//
+
+#include "MinimalTimeDifference.h"
+
+
+using namespace std;
+
+
+unsigned int ToMinutes(std::string time_HH_MM)
+{
+
+}
+unsigned int MinimalTimeDifference(std::vector<std::string> times)
+{
+
+}
