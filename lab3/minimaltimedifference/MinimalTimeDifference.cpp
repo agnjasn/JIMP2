a428028d85e6ca74b4e3057f63d9251956b26f50
biekin <kinga.kb@interia.pl>
2017-03-14 18:55:28 +0100

    lab3

--- lab3/minimaltimedifference/MinimalTimeDifference.cpp
+++ lab3/minimaltimedifference/MinimalTimeDifference.cpp
@@ -4,15 +4,52 @@
 
 #include "MinimalTimeDifference.h"
 
+#include <sstream>
+#include <regex>
+#include <cmath>
 
-using namespace std;
+namespace minimaltimedifference {
+    using namespace std;
+
+    unsigned int ToMinutes(std::string time_HH_MM) {
 
 
-unsigned int ToMinutes(std::string time_HH_MM)
-{
+        regex pattern {R"((\d{1,2}):\(d{2}))"};
+        smatch matches;
 
-}
-unsigned int MinimalTimeDifference(std::vector<std::string> times)
-{
+        regex_match(time_HH_MM, matches, pattern);
 
+        int h= atoi(static_cast<string>(matches[1]).c_str());
+        int m= atoi(static_cast<string>(matches[2]).c_str());
+
+        unsigned int minutes=60*h+m;
+        return minutes;
+    }
+
+    unsigned int MinimalTimeDifference(std::vector<std::string> times) {
+        unsigned int MinimalTimeDifference = 720;
+        vector<int> TimeInMinutes;
+        int diff = 0;
+
+        for (int i = 0; i < times.size(); i++) {
+            TimeInMinutes.push_back(ToMinutes(times[i]));
+        }
+
+        for (int i = 0; i < times.size() - 1; i++) {
+            for (int j = i + 1; j < times.size(); j++) {
+                diff = abs(TimeInMinutes[i] - TimeInMinutes[j]);
+
+                if (diff > 720) {
+                    diff = 1440 - diff;
+                }
+
+                if (diff < MinimalTimeDifference) {
+                    MinimalTimeDifference = diff;
+                }
+            }
+
+        }
+
+        return MinimalTimeDifference;
+    }
 }
--- lab3/minimaltimedifference/main.cpp
+++ lab3/minimaltimedifference/main.cpp
@@ -2,3 +2,19 @@
 // Created by Kinga on 14.03.2017.
 //
 
+#include "MinimalTimeDifference.h"
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+using namespace minimaltimedifference;
+
+int main (void)
+{
+    string s1="", s2="", s3="";
+    cin>>s1>>s2>>s3;
+    vector <string> v={s1, s2, s3};
+
+    cout<<MinimalTimeDifference(v);
+    return 0;
+}
