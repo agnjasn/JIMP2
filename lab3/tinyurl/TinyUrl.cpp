85990efca73d7ed1b0748347b80130ff58865b50
biekin <kinga.kb@interia.pl>
2017-03-19 23:12:03 +0100

    Merge branch 'master' of https://gitlab.com/agh-jimp2/exercises into lab3
    # Conflicts:
    #	tests/lab2test/CMakeLists.txt

--- lab3/tinyurl/TinyUrl.cpp
+++ lab3/tinyurl/TinyUrl.cpp
@@ -4,19 +4,43 @@
 
 #include "TinyUrl.h"
 
-std::unique_ptr<TinyUrlCodec> Init();
-{
+namespace tinyurl {
 
-}
-void NextHash(std::array<char, 6> *state)
-{
+    std::unique_ptr<TinyUrlCodec> Init() {
+        auto p = make_unique<TinyUrlCodec>(); //alokacja pamięci
+        p->hash.fill('0'); //wypełienie licznika pierwszym stanem
+        return p; //zwrócenie wskaźnika na strukturę z mapą z urlami i licznikiem do kodowania
+    }
 
-}
-std::string Encode(const std::string &url, std::unique_ptr<TinyUrlCodec> *codec)
-{
+    void NextDigit(char &digit) {   //generuje kolejną "cyfrę" 0 - 9 -> A -Z -> a - z
+        if (digit == '9') digit='A';
+        else if (digit == 'Z') digit='a';
+        else digit++;
+    }
 
-}
-std::string Decode(const std::unique_ptr<TinyUrlCodec> &codec, const std::string &hash)
-{
+    void NextHash(std::array<char, 6> *state) {  //kolejny ten do kodowania url, aktualny stan tego to będzie skórcony url
+        int i = 5;
+        while ((*state)[i] == 'z') {  //idzie po tej tablicy od tyłu i dopóki widzi 'z' to zamienia na '0'
+            (*state)[i] = '0'; // jak licznik jest samymi 'z'ami to się "przekręci" z powrotem na 000000
+            i--;
+        }
+        if (i >= 0) { // jak trafi na cos innego niz 'z' to zamienia ten znak na kolejną "cyfrę"
+            NextDigit((*state)[i]);  // według tego generatora 0-9A-Za-z
+        }
+
+    }
+
+    std::string Encode(const std::string &url, std::unique_ptr<TinyUrlCodec> *codec)
+    {
+        string tinyurl(begin((*codec)->hash), end((*codec)->hash)); //przerobienie tego licznika(?) z tej dziwnej tablicy na stringa
+        (*codec)->TinyUrlMap.emplace(tinyurl, url); // zapisanie w mapie skróconego i długiego url
+        return tinyurl; // zwrócenie tego zakodowanego url bo funkcja sobie tego życzy
+
+    }
+
+    std::string Decode(const std::unique_ptr<TinyUrlCodec> &codec, const std::string &hash)
+    {
+        return codec->TinyUrlMap[hash]; //no zwrócenie długiego urla powiązanego z danym hashem
+    }
 
 }
--- lab3/tinyurl/TinyUrl.h
+++ lab3/tinyurl/TinyUrl.h
@@ -9,16 +9,25 @@
 #include <string>
 #include <array>
 #include <memory>
+#include <map>
 
-using namespace std;
+namespace tinyurl
+{
+    using namespace std;
 
-struct TinyUrlCodec
-{};
+    struct TinyUrlCodec
+    {
+        map<string, string> TinyUrlMap; //mapa wiążąca skórcony url z jego długim odpowiednikiem
+        array<char, 6> hash; // licznik(?) którego stan będzie po prostu zakodowanym urlem
+    };
 
-std::unique_ptr<TinyUrlCodec> Init();
-void NextHash(std::array<char, 6> *state);
-std::string Encode(const std::string &url, std::unique_ptr<TinyUrlCodec> *codec);
-std::string Decode(const std::unique_ptr<TinyUrlCodec> &codec, const std::string &hash);
+    std::unique_ptr<TinyUrlCodec> Init();
 
+    void NextHash(std::array<char, 6> *state);
 
+    std::string Encode(const std::string &url, std::unique_ptr<TinyUrlCodec> *codec);
+
+    std::string Decode(const std::unique_ptr<TinyUrlCodec> &codec, const std::string &hash);
+
+}
 #endif //JIMP_EXERCISES_TINYURL_H
