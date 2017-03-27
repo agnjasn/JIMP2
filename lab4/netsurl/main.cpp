e92055cf78743c257a6e10ea5aeab682b5a87c5d
agnjasn <agnieszka.jasnosz07@gmail.com>
2017-03-27 17:44:21 +0200

    Json doesn't work
    Merge branch 'lab4' of https://github.com/agnjasn/jimp2 into lab4
    
    # Conflicts:
    #	lab4/geometry/Square.cpp
    #	lab4/geometry/Square.h

--- lab4/netsurl/main.cpp
+++ lab4/netsurl/main.cpp
@@ -3,7 +3,20 @@
 //
 
 #include "SimpleUrl.h"
+#include <regex>
+#include <iostream>
 int main()
 {
+    std::regex pat("\\{\\{\\w+\\}\\}");
+    std::string line {"hello {{it_s_me}}++{{aga}}"};
+    std::smatch matches;
+
+    std::cout << matches.size()<<std::endl;
+    int i=0;
+    while (regex_search(line, matches, pat)) {
+        std::cout << "udało się dopasować do linii: " << matches[i] << std::endl;
+        i++;
+        log = line.suffix();
+    }
     return 0;
 }
