095908ecd295fd41bd489f21df5da2cb6ee94fdd
agnjasn <agnieszka.jasnosz07@gmail.com>
2017-03-28 19:42:57 +0200

    lab5 xxx and uncompleted memorychunk

--- lab5/memorychunk/MemoryChunk.cpp
+++ lab5/memorychunk/MemoryChunk.cpp
@@ -3,7 +3,39 @@
 //
 
 #include "MemoryChunk.h"
-namespace memory
+namespace memorychunk
 {
 
+    MemoryChunk::MemoryChunk(size_t sz)
+    {
+        ptr=new int8_t[sz];
+    }
+    int8_t *MemoryChunk::MemoryAt(size_t offset) const
+    {
+
+    }
+    size_t MemoryChunk::ChunkSize() const
+    {
+
+    }
+
+    MemoryChunk::MemoryChunk(const MemoryChunk &m) {
+
+    }
+
+    MemoryChunk::MemoryChunk(MemoryChunk &&m) {
+
+    }
+
+    MemoryChunk &MemoryChunk::operator=(const MemoryChunk &m) {
+
+    }
+
+    MemoryChunk &MemoryChunk::operator=(MemoryChunk &&m) {
+
+    }
+
+    MemoryChunk::~MemoryChunk() {
+        delete []ptr;
+    }
 }
---
+++ lab5/xxx/movemain.cpp
@@ -0,0 +1,6 @@
+//
+// Created by agnieszka on 28.03.17.
+//
+#include "Xxx.h"
+
+
