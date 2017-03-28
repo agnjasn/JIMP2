3a50703eb9ab8b74da2b2446d69aca9c39bdb4c9
agnjasn <agnieszka.jasnosz07@gmail.com>
2017-03-28 12:11:35 +0200

    Simple Template Engine pass 8 of 8 tests without Injection
    Merge branch 'lab4' of https://github.com/agnjasn/jimp2 into lab4
    
    # Conflicts:
    #	lab4/geometry/Square.cpp
    #	lab4/geometry/Square.h

--- lab4/netstemplateengine/SimpleTemplateEngine.h
+++ lab4/netstemplateengine/SimpleTemplateEngine.h
@@ -20,17 +20,11 @@ namespace nets
     {
     public:
         View(const std::string &pattern);
-     //   void MakeTemplate (const std::string &pattern);
         std::string Render(const std::unordered_map <std::string, std::string> &model)const;
 
 
     private:
         std::string what_i_get;
-//        std::vector <std::string> parts;
-//        std::vector <std::string> blanks;
-
-   //     std::regex p {"\\{\\{([\w|_]+)\\}\\}"};
-
 
     };
 }
