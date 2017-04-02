8cac2dad4572936ff41959d17929b4e66dfdcfab
agnjasn <agnieszka.jasnosz07@gmail.com>
2017-04-02 13:42:20 +0200

    matrix add folders and functions

---
+++ lab5/matrix/Matrix.cpp
@@ -0,0 +1,68 @@
+//
+// Created by agnieszka on 02.04.17.
+//
+
+#include "Matrix.h"
+namespace algebra
+{
+
+    Matrix::Matrix() {
+
+    }
+
+    Matrix::Matrix(int rows, int col) {
+
+    }
+
+    Matrix::Matrix(const Matrix &m) {
+
+    }
+
+    Matrix::Matrix(const char *ch) {
+
+    }
+
+    Matrix::Matrix(std::string str) {
+
+    }
+
+    std::complex<double> Matrix::Get() const {
+        return std::complex<double>();
+    }
+
+    void Matrix::Set(std::complex<double> value) {
+
+    }
+
+    std::complex<double> **Matrix::add(std::complex<double> **m2) {
+        return nullptr;
+    }
+
+    std::complex<double> **Matrix::sub(std::complex<double> **m2) {
+        return nullptr;
+    }
+
+    std::complex<double> **Matrix::mul(std::complex<double> **m2) {
+        return nullptr;
+    }
+
+    std::complex<double> **Matrix::div(std::complex<double> **m2) {
+        return nullptr;
+    }
+
+    std::complex<double> **Matrix::mul(std::complex<double> value) {
+        return nullptr;
+    }
+
+    std::complex<double> **Matrix::div(std::complex<double> value) {
+        return nullptr;
+    }
+
+    std::complex<double> **Matrix::pow(int pot) {
+        return nullptr;
+    }
+
+    void Matrix::print(std::complex<double> **m) {
+
+    }
+}
