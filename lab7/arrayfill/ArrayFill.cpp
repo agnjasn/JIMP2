1efc6567f3453532e5e0d743ba8eb9a8c39930cf
agnjasn <agnieszka.jasnosz07@gmail.com>
2017-04-11 19:39:39 +0200

    arrayfill pass

--- lab7/arrayfill/ArrayFill.cpp
+++ lab7/arrayfill/ArrayFill.cpp
@@ -6,5 +6,27 @@
 
 namespace arrays
 {
+    void FillArray(size_t size, const ArrayFill &filler, std::vector<int> *v) {
+        v->clear();
+        v->reserve(size);
+        for (size_t i = 0; i < size; i++) {
+            v->emplace_back(filler.Value(i));
+        }
+    }
 
+    int UniformFill::Value(int index) const {
+        return value_;
+    }
+
+    int IncrementalFill::Value(int index) const {
+        return step_*index+value_;
+    }
+
+    int SquaredFill::Value(int index) const {
+        return step_*index*index+value_;
+    }
+
+    int RandomFill::Value(int index) const {
+        return (*distribution_)(*generate_);
+    }
 }
