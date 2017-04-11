58a9406db0f97802ee40f834c284a9f4348f4d68
biekin <kinga.kb@interia.pl>
2017-04-11 17:36:22 +0200

    studentrepository operators

---
+++ lab7/comparable/Comparable.h
@@ -0,0 +1,14 @@
+//
+// Created by Kinga on 11.04.2017.
+//
+
+#ifndef JIMP_EXERCISES_COMPARABLE_H
+#define JIMP_EXERCISES_COMPARABLE_H
+
+
+class Comparable {
+
+};
+
+
+#endif //JIMP_EXERCISES_COMPARABLE_H
