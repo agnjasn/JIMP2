7892b418054fe95674311704d867b4c7107c87ba
biekin <kinga.kb@interia.pl>
2017-04-27 13:44:33 +0200

    Klasa Ksztalt

---
+++ lab8/ballexception/BallException.h
@@ -0,0 +1,14 @@
+//
+// Created by  on 25.04.2017.
+//
+
+#ifndef JIMP_EXERCISES_BALLEXEPTION_H
+#define JIMP_EXERCISES_BALLEXEPTION_H
+
+
+class BallExeption {
+
+};
+
+
+#endif //JIMP_EXERCISES_BALLEXEPTION_H
---
+++ lab8/pesel/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by  on 25.04.2017.
+//
+#include "Pesel.h"
+#include <iostream>
+
+int main(void)
+{
+   // (const InvalidPeselLength &len);
+    return 0;
+}
