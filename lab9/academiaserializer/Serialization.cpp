c8932d7ec0ddd2a1ca5f861d081dafbfeb406c35
agnjasn <agnieszka.jasnosz07@gmail.com>
2017-05-15 00:11:39 +0200

    academia serializer

--- lab9/academiaserializer/Serialization.cpp
+++ lab9/academiaserializer/Serialization.cpp
@@ -3,3 +3,42 @@
 //
 
 #include "Serialization.h"
+
+namespace academia {
+    Serializer::Serializer(std::ostream *out_) : out_(out_) {}
+
+    Serializer::~Serializer() {
+
+    }
+
+    Serializer::Serializer() {}
+
+
+    Serializable::Serializable() {}
+
+    Serializable::~Serializable() {
+
+    }
+    std::string Room::EnumToString() const {
+        switch(type_) {
+            case Type::COMPUTER_LAB:
+                return "COMPUTER_LAB";
+            case Type::LECTURE_HALL:
+                return "LECTURE_HALL";
+            case Type::CLASSROOM:
+                return "CLASSROOM";
+        }
+
+
+    }
+    void Room::Serialize (Serializer *serial) const
+    {
+        serial->Header("Room");
+        serial->IntegerField("id", this->id_);
+        serial->StringField("name", this->name_);
+        serial->StringField("type", this->EnumToString());
+        serial->Footer("/Room");
+    }
+
+
+}
--- lab9/academiaserializer/Serialization.h
+++ lab9/academiaserializer/Serialization.h
@@ -5,10 +5,73 @@
 #ifndef JIMP_EXERCISES_SERIALIZATION_H
 #define JIMP_EXERCISES_SERIALIZATION_H
 
+#include <iostream>
+#include <vector>
 
-class Serialization {
+namespace academia
+{
+    class Serializer;
+    class Serializable {
+    public:
+        virtual ~Serializable();
 
-};
+        Serializable();
+
+        virtual void Serialize (Serializer *serial) const=0;
+    };
+
+    class Serializer {
+    public:
+        Serializer();
+
+        virtual ~Serializer();
+
+        virtual void IntegerField(const std::string &field_name, int value)=0;
+
+        virtual void DoubleField(const std::string &field_name, double value)=0;
+
+        virtual void StringField(const std::string &field_name, const std::string &value)=0;
+
+        virtual void BooleanField(const std::string &field_name, bool value)=0;
+
+        virtual void SerializableField(const std::string &field_name, const academia::Serializable &value)=0;
+
+        virtual void ArrayField(const std::string &field_name, const std::vector <std::reference_wrapper<const academia::Serializable>> &value)=0;
+
+        virtual void Header(const std::string &object_name)=0;
+
+        virtual void Footer(const std::string &object_name)=0;
+
+        Serializer(std::ostream *out_);
+    private:
+        std::ostream* out_;
+    };
+
+    class XmlSerializer: public Serializer{};
+
+    class Room: public Serializable
+    {
+    public:
+        enum class Type {
+            COMPUTER_LAB,
+            LECTURE_HALL,
+            CLASSROOM
+        };
+        std::string EnumToString() const;
+        Room() {}
+        Room(int id, std::string name, Type type): id_(id), name_(name), type_(type){}
+        void Serialize (Serializer *serial) const override ;
+
+    private:
+        int id_;
+        std::string name_;
+        Type type_;
+    };
+
+    class Building: public  Serializable
+    {};
 
 
+
+}
 #endif //JIMP_EXERCISES_SERIALIZATION_H
