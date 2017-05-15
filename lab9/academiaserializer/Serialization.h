c2570957feea8685a5e6eea4fe5dc3b0f37b4902
agnjasn <agnieszka.jasnosz07@gmail.com>
2017-05-15 18:25:50 +0200

    academia serializer

--- lab9/academiaserializer/Serialization.h
+++ lab9/academiaserializer/Serialization.h
@@ -76,6 +76,30 @@ namespace academia
 
     };
 
+    class JsonSerializer: public Serializer
+    {
+    public:
+        JsonSerializer(){};
+        virtual ~JsonSerializer(){};
+        JsonSerializer(std::ostream *out):Serializer(out){}
+        void IntegerField(const std::string &field_name, int value) override;
+
+        void DoubleField(const std::string &field_name, double value) override;
+
+        void StringField(const std::string &field_name, const std::string &value) override;
+
+        void BooleanField(const std::string &field_name, bool value) override;
+
+        void SerializableField(const std::string &field_name, const academia::Serializable &value) override;
+
+        void ArrayField(const std::string &field_name,
+                        const std::vector<std::reference_wrapper<const academia::Serializable>> &value) override;
+
+        void Header(const std::string &object_name) override;
+
+        void Footer(const std::string &object_name) override;
+    };
+
     class Room: public Serializable
     {
     public:
