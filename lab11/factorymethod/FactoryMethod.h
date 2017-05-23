e7849e84728c69a5f09c81290e1b72f1f0db6509
biekin <kinga.kb@interia.pl>
2017-05-23 16:21:53 +0200

    lab11

---
+++ lab11/factorymethod/FactoryMethod.h
@@ -0,0 +1,14 @@
+//
+// Created by Kinga on 23.05.2017.
+//
+
+#ifndef JIMP_EXERCISES_FACTORYMETHOD_H
+#define JIMP_EXERCISES_FACTORYMETHOD_H
+
+
+class FactoryMethod {
+
+};
+
+
+#endif //JIMP_EXERCISES_FACTORYMETHOD_H
---
+++ lab11/factorymethod/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Pomidor on 23.05.2017.
+//
+
+
+int main(void)
+{
+
+    return 0;
+}
