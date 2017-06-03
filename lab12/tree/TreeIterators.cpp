eeb1675aad9e07c631419c80737812a316674301
agnjasn <agnieszka.jasnosz07@gmail.com>
2017-06-04 00:00:58 +0200

    tree iterators

---
+++ lab12/tree/TreeIterators.cpp
@@ -0,0 +1,10 @@
+//
+// Created by agnieszka on 03.06.17.
+//
+
+#include "TreeIterators.h"
+#include "Tree.h"
+namespace tree
+{
+
+}
---
+++ lab12/tree/TreeIterators.h
@@ -0,0 +1,45 @@
+//
+// Created by agnieszka on 03.06.17.
+//
+
+#ifndef JIMP_EXERCISES_TREEITERATORS_H
+#define JIMP_EXERCISES_TREEITERATORS_H
+
+#include "Tree.h"
+
+namespace tree
+{
+    template <class T>
+    class InOrder
+    {
+        
+    };
+    template <class T>
+    class InOrderTreeIterator 
+    {
+        
+    };
+    template <class T>
+    class InOrderTreeView
+    {
+        
+    };
+    template <class T>
+    class PreOrder
+    {
+        
+    };
+    template <class T>
+    class PreOrderTreeIterator
+    {
+        
+    };
+    template <class T>
+    class PreOrderTreeView
+    {
+        
+    };
+}
+
+
+#endif //JIMP_EXERCISES_TREEITERATORS_H
