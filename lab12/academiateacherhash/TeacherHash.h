eb4e283f934faad7d1cc1817b205ad3e2523d327
agnjasn <agnieszka.jasnosz07@gmail.com>
2017-06-04 13:44:26 +0200

    teacherhash 11/11

--- lab12/academiateacherhash/TeacherHash.h
+++ lab12/academiateacherhash/TeacherHash.h
@@ -14,8 +14,9 @@ namespace academia
     public:
         TeacherId(){}
         TeacherId(int id_): id2(id_){}
-        int Id(){return id2;}
+        int Id() const {return id2;}
         operator int() {return id2;}
+    private:
         int id2;
     };
 
@@ -24,25 +25,25 @@ namespace academia
     public:
         Teacher(){}
         Teacher(TeacherId id_, std::string name_, std::string department_): id(id_), name(name_), department(department_){}
-        std::string Name(){return name;}
-        std::string Department(){ return department;}
-        TeacherId Id(){ return id;}
-
+        std::string Name() const {return name;}
+        std::string Department() const { return department;}
+        TeacherId Id() const { return id;}
+    private:
         std::string name;
         std::string department;
         TeacherId id;
     };
-    bool operator==(const TeacherId &t, const TeacherId &t2){return t2.id2==t.id2;}
-    bool operator==(const Teacher &t, const Teacher &t2){return t.id==t2.id && t.name==t2.name && t.department==t2.department;}
-    bool operator!=(const TeacherId &t, const TeacherId &t2){return t2.id2!=t.id2;}
-    bool operator!=(const Teacher &t, const Teacher &t2){return t.id!=t2.id || t.name!=t2.name || t.department!=t2.department;}
-    std::size_t operator*(TeacherId tea, size_t val){return tea.id2*val;}
+    bool operator==(const TeacherId &t, const TeacherId &t2){return t2.Id()==t.Id();}
+    bool operator==(const Teacher &t, const Teacher &t2){return t.Id()==t2.Id() && t.Name()==t2.Name() && t.Department()==t2.Department();}
+    bool operator!=(const TeacherId &t, const TeacherId &t2){return t2.Id()!=t.Id();}
+    bool operator!=(const Teacher &t, const Teacher &t2){return t.Id()!=t2.Id() || t.Name()!=t2.Name() || t.Department()!=t2.Department();}
+    std::size_t operator*(TeacherId tea, size_t val){return tea.Id()*val;}
     struct TeacherHash {
        std::size_t operator()(Teacher const &teacher1) const
        {
-           std::size_t h1 = std::hash<std::string>{}(teacher1.name);
-           std::size_t h2 = std::hash<std::string>{}(teacher1.department);
-           return (teacher1.id*h1+h2);
+           std::size_t h1 = std::hash<std::string>{}(teacher1.Name());
+           std::size_t h2 = std::hash<std::string>{}(teacher1.Department());
+           return (teacher1.Id()*h1+h2);
        }
     };
 
