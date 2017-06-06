09cbb638122dcfeb2913a1a312b056c9ddbc98c8
biekin <kinga.kb@interia.pl>
2017-06-06 19:11:03 +0200

    lab13

---
+++ lab13/arabicroman/ArabicRoman.h
@@ -0,0 +1,30 @@
+//
+// Created by kb on 06.06.17.
+//
+
+#ifndef JIMP_EXERCISES_ARABICROMAN_H
+#define JIMP_EXERCISES_ARABICROMAN_H
+
+#include <iostream>
+#include <string>
+#include<map>
+
+std::map<int, std::string> arabtoroman=
+        {
+                {1, "I"},
+                {2, "II"},
+                {3, "III"},
+                {4, "IV"},
+                {5, "V"},
+                {6, "VI"},
+        };
+
+
+std::string ArabicToRoman(int arab)
+{
+
+}
+
+
+
+#endif //JIMP_EXERCISES_ARABICROMAN_H
--- tests/lab13test/ArabicToRomanTest.cpp
+++ tests/lab13test/ArabicToRomanTest.cpp
@@ -2,3 +2,19 @@
 // Created by mwypych on 01.06.17.
 //
 
+#include <ArabicRoman.h>
+#include <gtest/gtest.h>
+
+TEST(ArabicRomanTests, OneShouldBeI)
+{
+    EXPECT_EQ("I", ArabicToRoman(1));
+}
+TEST(ArabicRomanTests, FiftyShouldBeL)
+{
+    EXPECT_EQ("L", ArabicToRoman(50));
+
+}
+TEST(ArabicRomanTests, Thousand589ShouldBeMDLXXXIX)
+{
+    EXPECT_EQ("MDLXXXIX", ArabicToRoman(1589));
+}
